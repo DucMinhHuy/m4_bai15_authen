e959993514dcf45cf73b035f33dc9cded3b13672
DucMinhHuy <ducminhhuy99tbvn2@gmail.com>
2022-11-02 19:47:56 +0700

    author

---
+++ thuchanh/src/main/java/huy/thuchanh/config/AppInit.java
@@ -0,0 +1,21 @@
+package huy.thuchanh.config;
+
+import huy.thuchanh.security.SecurityConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SecurityConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{AppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ thuchanh/src/main/java/huy/thuchanh/security/SecurityInit.java
@@ -0,0 +1,6 @@
+package huy.thuchanh.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
